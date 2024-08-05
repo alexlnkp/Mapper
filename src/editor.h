bf86a22468ce9ec71d3c8aca381064a3711f5c1a
Alex Murkoff <413x1nkp@gmail.com>
2024-08-06 05:36:24 +0700

    docs: add comments to the Cube struct

--- src/editor.h
+++ src/editor.h
@@ -17,8 +17,8 @@ typedef enum CameraState {
 } CameraState;
 
 typedef struct {
-    Vector3 pos;
-    Vector3 dim;
+    Vector3 pos; /* Center point of the cube in the world space */
+    Vector3 dim; /* Dimensionality of the cube; x for width, y for height, z for length */
     Color col;
 } Cube;
 
